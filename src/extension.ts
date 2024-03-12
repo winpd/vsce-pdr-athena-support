ab01c00b8cc0c29682c5aed528e8a9ad8c89869f
winpd <>
2024-03-12 17:41:52 +0900

    Revert "프로젝트 환경 셋팅"
    This reverts commit 9174f7edbd47e58e980e36087c7e5cdedf1dda04.

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('rs.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from rathena!');
+		vscode.window.showInformationMessage('Hello World from rAthena!');
 	});
 
 	context.subscriptions.push(disposable);
