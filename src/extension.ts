1dacb26b93de86d1b72bd2a3f06249c373e51ed5
winpd <>
2024-03-13 17:31:19 +0900

    feat: 파일 인코딩 처리 및 안내 메시지 작업

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@
 import * as vscode from 'vscode';
 import { isRathenaHeader } from './rathena';
 import { CompletionProvider } from './completionProvider';
-import { FileEncodingManager } from './encode/fileEncodingManager';
+import { checkFileAndShowPopup } from './encode/checkfile';
 
 function associateFile (doc: vscode.TextDocument): void {
   if (isRathenaHeader(doc.lineAt(0).text)) {
@@ -13,30 +13,23 @@ function associateFile (doc: vscode.TextDocument): void {
 
 export function activate (context: vscode.ExtensionContext): void {
   const provider = new CompletionProvider();
-  const triggerCharacters = ['set'];
 
   //파일 스니펫 및 context 처리
   for (const doc of vscode.workspace.textDocuments) {
     associateFile(doc);
   }
 
-  //파일 인코딩 처리
-  let convertEncodingCommand = vscode.commands.registerCommand('extension.convertEncoding', () => {
-    if (vscode.window.activeTextEditor) {
-        FileEncodingManager.convertFileEncodingIfUTF8(vscode.window.activeTextEditor.document.fileName);
-    }
-  });
-
-  let convertSpacesToTabsCommand = vscode.commands.registerCommand('extension.convertSpacesToTabs', () => {
-      FileEncodingManager.convertSpacesToTabsInActiveFile();
-  });
-
   // Also associate file on open and save
   context.subscriptions.push(vscode.workspace.onDidOpenTextDocument(associateFile));
   context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(associateFile));
 
-  context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ language: 'rathena', scheme: 'file' }, provider, ...triggerCharacters));
-  context.subscriptions.push(convertEncodingCommand, convertSpacesToTabsCommand);
+  context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ language: 'rathena', scheme: 'file' }, provider));
+  
+  context.subscriptions.push(vscode.workspace.onDidOpenTextDocument(document => {
+    if (isRathenaHeader(document.lineAt(0).text) || (document.fileName.endsWith('.rs') || document.fileName.endsWith('.ers'))) {
+      checkFileAndShowPopup(document);
+    }
+  }));
 }
 
 export function deactivate (): void {
